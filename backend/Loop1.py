1e85c135c202c51c2ea61002c97e6124c55c20c3
Macharia <126384931+mashaamacharia@users.noreply.github.com>
2024-10-30 10:13:44 +0300

    Add files via upload

---
+++ backend/Loop1.py
@@ -0,0 +1,9 @@
+from Python.lesson4 import scores
+
+while True:
+    print("hello loops")
+    break
+scorez = [90,70,60,45,34,67,34,67,89,]
+for score in scorez:
+    if score >=50 and score <=70 and score %2==0:
+       print(score)
