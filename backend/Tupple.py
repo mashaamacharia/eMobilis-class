4b421ce40c88bc5422f17eefaf7ab16483cecf3e
Macharia <126384931+mashaamacharia@users.noreply.github.com>
2024-10-31 10:07:51 +0300

    Add files via upload

---
+++ backend/Tupple.py
@@ -0,0 +1,8 @@
+#Tuples
+#Immutble -- Cannot change
+
+days_of_the_week = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+print(days_of_the_week[0])
+print(days_of_the_week)
+
+print(len(days_of_the_week))
