1d48d0b259425bd0a43a6ab116035b1a8031ae4d
Macharia <126384931+mashaamacharia@users.noreply.github.com>
2024-11-02 11:14:43 +0300

    Add files via upload

---
+++ backend/cubeVolume.py
@@ -0,0 +1,5 @@
+a=float(input("ENter the base of the cube"))
+b=float(input("Enter the legth of the cube"))
+c=float(input("Enter the height of the cube"))
+volume=a*b*c
+print("The voluime of the cube is ",volume)
