566442bfdd1e387edd82e7ab7f7013578750852f
Macharia <126384931+mashaamacharia@users.noreply.github.com>
2024-11-03 13:15:11 +0300

    Add files via upload

---
+++ backend/week.py
@@ -0,0 +1,17 @@
+num1 =int(input("Enter the number of your choice"))
+if num1=='1':
+        print("Sunday")
+elif num1=='2':
+        print("Monday")
+elif num1==3:
+        print("Tuesday") 
+elif num1==4:
+        print("Wednesday")
+elif num1==5:
+        print('Thursday')
+elif num1==6:
+        print("Friday")
+elif num1==7:
+        print("Saturday")
+else:
+        print("INVALID OUTPUT")   
